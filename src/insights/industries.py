caf1f417235f45055f30572f0baf01d9530527ae
Melania P Chagas <melaniapchagas@gmail.com>
2023-02-21 19:50:22 -0300

    feat: Implementa função 'get_unique_industries'

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,23 +1,16 @@
 from typing import List, Dict
+from src.insights.jobs import (read)
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    job_list = read(path)
+    industries = []
+    for job in job_list:
+        if job['industry'] not in industries and job['industry'] != '':
+            industries.append(job['industry'])
+    return industries
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
-
+# print(get_unique_industries('data/jobs.csv'))
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     """Filters a list of jobs by industry
