932014fa114cc48e9b21d724ab342bbe2d2cc87f
Melania P Chagas <melaniapchagas@gmail.com>
2023-02-24 17:53:01 -0300

    feat: Implementa a função 'filter_by_job_type'

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -26,18 +26,14 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
+    if job_type == '':
+        return []
+    jobs_filtred = []
+    for job in jobs:
+        if job['job_type'] == job_type:
+            jobs_filtred.append(job)
+    return jobs_filtred
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+# jobs = read('data/jobs.csv')
+# print(filter_by_job_type(jobs, ''))
